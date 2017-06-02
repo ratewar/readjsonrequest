8be2c12c594ceda85c8cf025f181bcaf8e27378c
ratewar <rajatratewal@gmail.com>
2017-06-02 14:07:43 +0530

    Fixed issues

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # readjsonrequest
 npm package to read the JSON link
+
+# To install
+npm install readjsonrequest
+
+# To use
+var getreq = require('readjsonrequest');
+getreq('https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json',
+ function(error,obj)
+ {
+   console.log(obj);
+});
--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-var argv = require('yargs')
-           .usage("usage: node $0 --url=[String]")
-					 .example('$0 --url "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json"', 'Read JSON Data')
-           .demand(['url'])
-           .argv;
-
 var request = require("request");
 
-module.exports.readjsonrequest = function(urlpath, callback) {
-
+module.exports = function readjsonrequest(myurl,callback)
+{
 	request({
-	    url: argv.url,
-	    json: true
-	}, function (error, response, body) {
-			//if the response code is ok
-	    if (!error && response.statusCode === 200) {
-	    		callback(null,{body});
-	    } else {
-					callback(error,null);
-				}
-	});
-
-}
+	    			url: myurl,
+	    			json: true
+					}, function (error, response, body) {
+									if (!error && response.statusCode === 200) {
+						 			callback(null,body);
+								}
+	    });
+};
